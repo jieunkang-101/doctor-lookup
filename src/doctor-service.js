9e15d6266908bf8a5b751802d2e8bfe0e3a5dfd3
Jieun Kang <jieunkang101@gmail.com>
2020-02-14 15:21:11 -0800

    add new API for searching for doctors by name

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -13,4 +13,22 @@ export class Doctor {
       return false;
     }
   }
+
+  async getDoctorByName(name) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.512230%2C-122.658722%2C100&sort=distance-asc&limit=25&user_key=${process.env.API_KEY}`);
+      let jsonfiedResponse;
+      if (response.ok && response.status === 200) {
+        jsonfiedResponse = await response.json();
+      } else {
+        jsonfiedResponse = false;
+      }
+      return jsonfiedResponse;
+    } catch(error) {
+      return false;
+    }
+  }
+
 }
+
+
