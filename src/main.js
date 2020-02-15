cf3ecbfe0252aa2ec899a42025f7da0fbdc05b7f
Jieun Kang <jieunkang101@gmail.com>
2020-02-14 16:10:39 -0800

    add refresh link

--- src/main.js
+++ src/main.js
@@ -23,17 +23,18 @@ $(document).ready(function() {
     const getElementsByIssue = function(issueResponse) {
 
       if (issueResponse === false) {
-        $(".errorOutput").append(`There was an error handling your request. <a href='index.html'>Click here to try again</a>`);
+        $(".errorOutput").append(`There was an error handling your request: ${issueResponse.error} <br><a href='index.html'>Click here to try again</a>`);
       } else if (issueResponse.meta.count > 0) {
         issueResponse.data.forEach(function(data) {
           $(".output").show();
           $(".sentence").text(`A list of doctors who treats ${issue}.`);
           $(".outputResult").append("<li>" + data.profile.title + ". " + data.profile.first_name + " " + data.profile.last_name + "</li>" + "<ul>" + "<li>" + "Phone Number: " + data.practices[0].phones[0].number + "</li>" + "<li>" + "Address: " + data.practices[0].visit_address.street + ", " + data.practices[0].visit_address.city + ", " + data.practices[0].visit_address.state + ", " + data.practices[0].visit_address.zip + "</li>" + "<li>" + "Accept New Patients: " + data.practices[0].accepts_new_patients + "</li>" + "<li>" + "Website: " + "<a href='" + data.practices[0].website + "'>" + data.practices[0].website + "</a>" +"</li>" + "</ul>" + "<br>" + "</li>");
         });
-      } else {
-        $(".noResult").append("<h5>Unfortunately, There are no doctors meet your search criteria in Portland area</h5>");
-      }
-    
-    };
+        $(".refresh").append(`<a href='index.html'>Click here to try again</a>`);
+        } else {
+          $(".noResult").append("Unfortunately, There are no doctors meet your search criteria in Portland area");
+          $(".refresh").append(`<a href='index.html'>Click here to try again</a>`);
+      }        
+    }
   });
 });
