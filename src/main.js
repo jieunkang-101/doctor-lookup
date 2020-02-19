149d7f4cb98b4609e0a3621cc021cae497035406
Jieun Kang <jieunkang101@gmail.com>
2020-02-19 12:38:00 -0800

    edit error message and returned information for website

--- src/main.js
+++ src/main.js
@@ -16,17 +16,21 @@ $(document).ready(function() {
       let doctorSearch = new Doctor();
       const issueResponse = await doctorSearch.getDoctorByIssue(issue);
       getElementsByIssue(issueResponse);
+      console.log(issueResponse);
     })();
 
     const getElementsByIssue = function(issueResponse) {
-
-      if (issueResponse === false) {
-        $(".errorOutput").append(`There was an error handling your request: ${issueResponse.error} <br><a href='index.html'>Click here to try again</a>`);
+      if (typeof(issueResponse) == "string") {
+        $(".errorOutput").append(`There was an error handling your request: ${issueResponse} <br><a href='index.html'>Click here to try again</a>`);
       } else if (issueResponse.meta.count > 0) {
-        issueResponse.data.forEach(function(data) {
+          issueResponse.data.forEach(function(data) {
           $(".output").show();
           $(".sentence").text(`A list of doctors who treats ${issue}.`);
-          $(".outputResult").append("<li>" + data.profile.title + ". " + data.profile.first_name + " " + data.profile.last_name + "</li>" + "<ul>" + "<li>" + "Phone Number: " + data.practices[0].phones[0].number + "</li>" + "<li>" + "Address: " + data.practices[0].visit_address.street + ", " + data.practices[0].visit_address.city + ", " + data.practices[0].visit_address.state + ", " + data.practices[0].visit_address.zip + "</li>" + "<li>" + "Accept New Patients: " + data.practices[0].accepts_new_patients + "</li>" + "<li>" + "Website: " + "<a href='" + data.practices[0].website + "'>" + data.practices[0].website + "</a>" +"</li>" + "</ul>" + "<br>" + "</li>");
+          if (data.practices[0].website == null) {
+            $(".outputResult").append("<li>" + data.profile.title + ". " + data.profile.first_name + " " + data.profile.last_name + "</li>" + "<ul>" + "<li>" + "Phone Number: " + data.practices[0].phones[0].number + "</li>" + "<li>" + "Address: " + data.practices[0].visit_address.street + ", " + data.practices[0].visit_address.city + ", " + data.practices[0].visit_address.state + ", " + data.practices[0].visit_address.zip + "</li>" + "<li>" + "Accept New Patients: " + data.practices[0].accepts_new_patients + "</li>" + "<li>" + "Website: there is no website" + "</li>" + "</ul>" + "<br>" + "</li>");
+          } else {
+            $(".outputResult").append("<li>" + data.profile.title + ". " + data.profile.first_name + " " + data.profile.last_name + "</li>" + "<ul>" + "<li>" + "Phone Number: " + data.practices[0].phones[0].number + "</li>" + "<li>" + "Address: " + data.practices[0].visit_address.street + ", " + data.practices[0].visit_address.city + ", " + data.practices[0].visit_address.state + ", " + data.practices[0].visit_address.zip + "</li>" + "<li>" + "Accept New Patients: " + data.practices[0].accepts_new_patients + "</li>" + "<li>" + "Website: " + "<a href='" + data.practices[0].website + "'>" + data.practices[0].website + "</a>" +"</li>" + "</ul>" + "<br>" + "</li>");
+          }
         });
         $(".refresh").append(`<a href='index.html'>Click here to try again</a>`);
       } else {
@@ -50,13 +54,17 @@ $(document).ready(function() {
 
     const getElementsByName = function(nameResponse) {
 
-      if (nameResponse === false) {
-        $(".errorOutput").append(`There was an error handling your request: ${nameResponse.error} <br><a href='index.html'>Click here to try again</a>`);
+      if (typeof(nameResponse) == "string") {
+        $(".errorOutput").append(`There was an error handling your request: ${nameResponse} <br><a href='index.html'>Click here to try again</a>`);
       } else if (nameResponse.meta.count > 0) {
-        nameResponse.data.forEach(function(data) {
+          nameResponse.data.forEach(function(data) {
           $(".output").show();
           $(".sentence").text(`A list of doctors with the name of ${name}.`);
-          $(".outputResult").append("<li>" + data.profile.title + ". " + data.profile.first_name + " " + data.profile.last_name + "</li>" + "<ul>" + "<li>" + "Phone Number: " + data.practices[0].phones[0].number + "</li>" + "<li>" + "Address: " + data.practices[0].visit_address.street + ", " + data.practices[0].visit_address.city + ", " + data.practices[0].visit_address.state + ", " + data.practices[0].visit_address.zip + "</li>" + "<li>" + "Accept New Patients: " + data.practices[0].accepts_new_patients + "</li>" + "<li>" + "Website: " + "<a href='" + data.practices[0].website + "'>" + data.practices[0].website + "</a>" +"</li>" + "</ul>" + "<br>" + "</li>");
+          if (data.practices[0].website == null) {
+            $(".outputResult").append("<li>" + data.profile.title + ". " + data.profile.first_name + " " + data.profile.last_name + "</li>" + "<ul>" + "<li>" + "Phone Number: " + data.practices[0].phones[0].number + "</li>" + "<li>" + "Address: " + data.practices[0].visit_address.street + ", " + data.practices[0].visit_address.city + ", " + data.practices[0].visit_address.state + ", " + data.practices[0].visit_address.zip + "</li>" + "<li>" + "Accept New Patients: " + data.practices[0].accepts_new_patients + "</li>" + "<li>" + "Website: there is no website" + "</li>" + "</ul>" + "<br>" + "</li>");
+          } else {
+            $(".outputResult").append("<li>" + data.profile.title + ". " + data.profile.first_name + " " + data.profile.last_name + "</li>" + "<ul>" + "<li>" + "Phone Number: " + data.practices[0].phones[0].number + "</li>" + "<li>" + "Address: " + data.practices[0].visit_address.street + ", " + data.practices[0].visit_address.city + ", " + data.practices[0].visit_address.state + ", " + data.practices[0].visit_address.zip + "</li>" + "<li>" + "Accept New Patients: " + data.practices[0].accepts_new_patients + "</li>" + "<li>" + "Website: " + "<a href='" + data.practices[0].website + "'>" + data.practices[0].website + "</a>" +"</li>" + "</ul>" + "<br>" + "</li>");
+          }
         });
         $(".refresh").append(`<a href='index.html'>Click here to try again</a>`);
       } else {
